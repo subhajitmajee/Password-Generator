0bff8a43e2e803b0527bd804426656c02d1287a4
Subhajit Majee <94911109+subhajitmajee@users.noreply.github.com>
2023-11-27 17:50:17 +0530

    Add files via upload

---
+++ Random_Password_Generator.py
@@ -0,0 +1,5 @@
+import random
+passlen = int(input("Enter the length of password:\t"))
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p =  "".join(random.sample(s,passlen ))
+print (p)
